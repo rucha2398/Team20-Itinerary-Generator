de3888c5bb71cde55dbd58795a4ae266d1ff761c
mkamesam <kamesam.m@husky.neu.edu>
2019-10-30 18:26:52 -0400

    set up results page & added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ItineraryResult from './components/ItineraryResults';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Hi changed some stufffff
-        </p>
-      </header>
+      <ItineraryResult/>
     </div>
   );
 }
---
+++ src/components/ItineraryResults.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../styles/ItineraryResults.css';
+
+export default class ItineraryResults extends React.Component {
+    render() {
+        return (
+            <h1> results page </h1>
+        )
+    }
+}
