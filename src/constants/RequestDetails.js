5bae3abb18a793809ed8fa60b7a30f19c72fea67
edgarjoel21 <maldonado.ed@husky.neu.edu>
2019-12-07 15:37:38 -0500

    Request details page

---
+++ src/constants/RequestDetails.js
@@ -0,0 +1,12 @@
+const RequestDetails = (request) => (
+    <div>
+        <h4>Sample Request</h4>
+        <ul>
+            <li>Sample details</li>
+            <li>Sample details</li>
+            <li>Sample details</li>
+        </ul>
+    </div>
+)
+
+export default RequestDetails;
